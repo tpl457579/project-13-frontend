18a8b03c19e316fb35e252e7f1653fd8ca7c61b7
thomas-ludden <tpl457579@gmail.com>
2026-02-25 16:23:22 +0100

    final fixes

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   <div className='home'>
     <img
       className='home-img'
-      src='https://www.thevetgal.com/blog/wp-content/uploads/2023/09/iStock-1598193080-1-1500x1000.jpg'
+      src='https://pub-f4963f8b687346bf92cccfc72ef77667.r2.dev/zendocs-bucket-production/captured-files/checkout/compressed-home-img-039350a0-3abf-4719-b9f2-4f24298f06da.jpg'
       alt='Cute Pets'
     />
     <h1 className='home-h1'>Welcome Pet Lovers!</h1>
